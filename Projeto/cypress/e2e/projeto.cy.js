f3ea27fe52746d0c48c93f0407b438e1b4e37c4f
Lucas Belino <lucas23belino@gmail.com>
2023-11-14 21:02:14 -0300

    Projeto Reinsercao

---
+++ Projeto/cypress/e2e/projeto.cy.js
@@ -0,0 +1,10 @@
+describe('Cenários de tetse para o Swag Labs', () => {
+
+
+  it('Caso de teste1: Testando a tela de login do site', () => {
+    cy.visit('https://www.saucedemo.com')
+    
+  })
+
+
+})
